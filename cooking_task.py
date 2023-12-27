01e76f3e475e730bd3251780fcb0821a0215e456
Henry Tirla <henrytirla@gmail.com>
2023-12-27 15:39:30 +0100

    intro

---
+++ cooking_task.py
@@ -0,0 +1,22 @@
+#Intuitive idea of async programming- Similar scenario can be used to monitor lp pools on Univswap V2/V3
+
+#Port this to pools monitoring script
+
+import asyncio
+
+async def cook_dish(name, time):
+    print(f"Start cooking {name}")
+    await asyncio.sleep(time)
+    print(f"{name} is ready")
+
+async def main():
+    # Create tasks for different dishes
+    dish1 = cook_dish("Pasta", 3)
+    dish2 = cook_dish("Steak", 5)
+    dish3 = cook_dish("Salad", 2)
+
+    # Start the event loop to coordinate the tasks
+    await asyncio.gather(dish1, dish2, dish3)
+
+# Run the main function
+asyncio.run(main())
